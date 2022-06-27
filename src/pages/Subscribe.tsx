c20bcd7bbac35c7f692c3fe6632f11261e081c28
Dark Nite <laylaventilari5@gmail.com>
2022-06-27 00:33:40 -0300

    commit

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,28 +1,22 @@
-import { gql, useMutation } from "@apollo/client";
+
 import { useState, FormEvent} from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 
-
-export function Subscribe(){
-    const navigate = useNavigate()
+const Subscribe = () => {
+    const navigate = useNavigate();
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
-    const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+    const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
-async function handleSubscribe(event: FormEvent){
-        event.preventDefault();
-
-    await createSubscriber({
-            variables:{
-                name,
-                email,
-            }
-        }) 
-        navigate('/event')
-    }
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await createSubscriber({ variables: { name, email } });
+        navigate("/event");
+      };
 
     return (
             <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
@@ -43,7 +37,10 @@ async function handleSubscribe(event: FormEvent){
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded">
                     <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
 
-                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                    <form
+                        onSubmit={(e) => handleSubmit(e)}
+                        className="flex flex-col gap-2 w-full"
+                    >
                         <input 
                             className="bg-gray-900 rounded px-5 h-14"
                             type="text"
