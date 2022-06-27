624ecc9b3a8e730765cdd9027d23b239769d9160
Dark Nite <laylaventilari5@gmail.com>
2022-06-27 00:05:55 -0300

    commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,9 @@
 import { Lesson } from "./Lesson";
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 
-const GET_LESSONS_QUERY = gql`
-query {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}
-
-`
-
-interface GetLessonsQueryResponse{
-    lessons:{
-        id:string
-        title: string
-        slug:string
-        availableAt:string
-        lessonType: 'live' | 'class'
-    }[]
-}
 
 export function Sidebar(){
-    const {data} = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+    const { data } = useGetLessonsQuery()
     
     return(
 
@@ -48,5 +26,5 @@ export function Sidebar(){
                 
             </div>
         </aside>
-    )
-    }
+    );
+}
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,14 +3,6 @@ import { useState, FormEvent} from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 
-const CREATE_SUBSCRIBER_MUTATION = gql`
-    mutation CreateSubscriber($name: String!, $email: String!) {
-        createSubscriber(data: {name: $name, email: $email}) {
-            id
-    }
-}
-
-`
 
 export function Subscribe(){
     const navigate = useNavigate()
@@ -18,7 +10,7 @@ export function Subscribe(){
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
-    const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+    const [createSubscriber, {loading}] = useMutation(CREATE_SUBSCRIBER_MUTATION)
 
 async function handleSubscribe(event: FormEvent){
         event.preventDefault();
@@ -67,7 +59,8 @@ async function handleSubscribe(event: FormEvent){
 
                         <button
                         type="submit"
-                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+                        disabled={loading}
+                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
                         >
                             Garantir minha vaga
                         </button>
